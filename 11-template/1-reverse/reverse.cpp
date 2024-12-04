198ee713cc1407d7ea9d3c49771c90076148c174
Zoltán Porkoláb <zporky@gmail.com>
2024-12-04 23:55:05 +0100

    Small fix on reverse.

--- 11-template/1-reverse/reverse.cpp
+++ 11-template/1-reverse/reverse.cpp
@@ -8,19 +8,19 @@ int main()
   std::vector<int>    vi2 = { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
   std::vector<int>    vi3 = { 21, 22, 23, 24, 25, 26, 27, 28, 29};
   std::vector<double> vd  = { 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1, 8.1, 9.1};
-  std::vector<std::vector<int>> vv = { vi1, vi2, vi3 };
+//  std::vector<std::vector<int>> vv = { vi1, vi2, vi3 };
 
   reverse(vi1);
   reverse(vi2);
   reverse(vi3);
   reverse(vd);
-  reverse(vv);
+//  reverse(vv);
 
   print(std::cout,vi1);
   print(std::cout,vi2);
   print(std::cout,vi3);
   print(std::cout,vd);
-  print(std::cout,vv);
+//  print(std::cout,vv);
 
   return 0;
 }
